43882fff4043bf39adf79ca5b84a9131c5bd3d6d
Simon Rydberg <simpor81@gmail.com>
2024-12-04 21:03:14 +0100

    day 4

--- year2024/src/main/kotlin/year2024/day01/Solution202404.kt
+++ year2024/src/main/kotlin/year2024/day01/Solution202404.kt
@@ -5,47 +5,60 @@ import utils.AoCUtils.test
 import utils.Map2d
 import utils.Map2dDirection
 import utils.Point
-import utils.parseMap
 import utils.around
+import utils.parseMap
 
 fun main() {
+    val dirs = listOf(
+        Map2dDirection.N,
+        Map2dDirection.NE,
+        Map2dDirection.E,
+        Map2dDirection.SE,
+        Map2dDirection.S,
+        Map2dDirection.SW,
+        Map2dDirection.W,
+        Map2dDirection.NW
+    )
 
     val XMAS = "XMAS"
 
-    fun wordSearch(pos: Point, nextChar: Char, map: Map2d<Char>): Long {
+    fun wordSearch(pos: Point, dir: Map2dDirection, nextChar: Char, map: Map2d<Char>): Long {
         if (map[pos] != nextChar) return 0
         if (map[pos] == 'S') return 1
         val next = XMAS[XMAS.indexOf(nextChar) + 1]
-        return map.around(
-            pos,
-            listOf(
-                Map2dDirection.N,
-                Map2dDirection.NE,
-                Map2dDirection.E,
-                Map2dDirection.SE,
-                Map2dDirection.S,
-                Map2dDirection.SW,
-                Map2dDirection.W,
-                Map2dDirection.NW
-            )
-        )
-            .map {
-                wordSearch(it.key, next, map)
-            }.sum()
+        return map.around(pos, listOf(dir)).map { wordSearch(it.key, dir, next, map) }.sum()
     }
 
     fun part1(input: String, debug: Boolean = false): Long {
-
         val map = parseMap(input) { i -> i }
 
-        return map.keys
-            .map { wordSearch(it, 'X', map)
-            }.sum().toLong()
+        return map.keys.map { pos ->
+            dirs.map { dir ->
+                wordSearch(pos, dir, 'X', map)
+            }.sum()
+
+        }.sum().toLong()
 
     }
 
     fun part2(input: String, debug: Boolean = false): Long {
-        return -1L
+        val map = parseMap(input) { i -> i }
+
+        return map.keys
+            .filter { pos -> map[pos] == 'A' }
+            .map { pos ->
+                val dir1 = listOf(Map2dDirection.NW, Map2dDirection.SE)
+                val dir2 = listOf(Map2dDirection.SW, Map2dDirection.NE)
+
+
+                val str2 = map.around(pos, dir2).map { it.value }.sorted().joinToString()
+                val str1 = map.around(pos, dir1).map { it.value }.sorted().joinToString()
+
+//                println("str1: $str1 -- str2: $str2")
+                if (str2 == "M, S" && str1 == "M, S") 1
+                else 0
+            }.sum().toLong()
+
     }
 
     val testInput = "MMMSXXMASM\n" +
@@ -62,8 +75,9 @@ fun main() {
     val input = AoCUtils.readText("file202404.txt")
 
     part1(testInput) test Pair(18L, "test 1 part 1")
-    part1(input) test Pair(0L, "part 1")
+    part1(input) test Pair(2521L, "part 1")
 
-    part2(testInput) test Pair(0L, "test 2 part 2")
-    part2(input) test Pair(0L, "part 2")
+    part2(testInput) test Pair(9L, "test 2 part 2")
+    part2(input) test Pair(1912L, "part 2")
+    // 1964 to high
 }
